53f909bbbeec917ddf561335eb97a669ba8d903d
TatsianaKobets <kobets.tatsiana@gmail.com>
2024-11-13 13:38:55 +0300

    Refactoring. Adding Docker and Liquibase.

---
+++ src/main/java/by/http/exchangerparser/repository/MoneyRepository.java
@@ -0,0 +1,7 @@
+package by.http.exchangerparser.repository;
+
+import by.http.exchangerparser.entity.Money;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MoneyRepository extends JpaRepository<Money, Long> {
+}
--- src/main/java/by/http/task1/repository/MoneyRepository.java
+++
@@ -1,7 +0,0 @@
-package by.http.task1.repository;
-
-import by.http.task1.entity.Money;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MoneyRepository extends JpaRepository<Money, Long> {
-}
